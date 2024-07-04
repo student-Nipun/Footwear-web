7db2b672a379a045e1126af3a750ca382f7085af
student-Nipun <nipungoyal83909@gmail.com>
2024-07-04 16:34:06 +0530

    Initial commit

---
+++ c++.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*int n;
+    cout << "Enter n" << endl;
+    cin >> n;
+    while (n > 0)
+    {
+        cout << n << endl;
+        cin >> n;
+    }*/
+
+    cout << "Hello world" << endl;
+    int ascii = 'a';
+    cout << ascii;
+
+    return 0;
+}
